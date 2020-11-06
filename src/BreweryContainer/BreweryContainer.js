2977f93764c439713a21785940faa31c73a36a2d
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-06 16:40:07 -0700

    Create BreweryContainer class component with initial render

---
+++ src/BreweryContainer/BreweryContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './BreweryContainer.css';
+
+class BreweryContainer extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <h3>BreweryContainer goes here</h3>
+    )
+  }
+}
+export default BreweryContainer;
