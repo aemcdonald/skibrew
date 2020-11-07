17d698ad3242002b629142814231dc06ec32a5b9
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-07 14:37:34 -0700

    Write unit test for ResortCard information display

--- src/ResortCard/ResortCard.test.js
+++ src/ResortCard/ResortCard.test.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ResortCard from './ResortCard';
+import { BrowserRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe('ResortCard', () => {
-  it('Should pass an initial test', () => {
-    expect(true).toBe(true);
+  it('Should render a card with the correct information', () => {
+  const { getByText } = render(
+      <BrowserRouter>
+        <ResortCard
+          key={1}
+          id={1}
+          name={'Breckenridge'}
+        />
+      </BrowserRouter>
+    )
+
+    const resortName = getByText('Breckenridge')
+    expect(resortName).toBeInTheDocument();
   })
 });
