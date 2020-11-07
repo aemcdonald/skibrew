9b06be1f34458c52665166fb317a7860e76db88e
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-06 17:37:13 -0700

    Fetch breweries in componentDidMount & set state

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import ResortCard from '../ResortCard/ResortCard';
 import BreweryContainer from '../BreweryContainer/BreweryContainer';
+import { getAllBreweries } from '../apiCalls'
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      breweries: []
+    }
+  }
+
+  componentDidMount = async () => {
+    const allBreweries = await getAllBreweries()
+    this.setState({ breweries: allBreweries.flat()})
+    console.log(allBreweries.flat());
   }
 
   render() {
