9ae122db4b3aeba0cd67d58fd1027b21ab5f7123
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-06 22:11:14 -0700

    Format phone number

--- src/BreweryCard/BreweryCard.js
+++ src/BreweryCard/BreweryCard.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './BreweryCard.css';
 
 const BreweryCard = (props) => {
-  console.log(props.brewery);
   return(
     <section>
       <h4>{props.brewery.name}</h4>
-        <p>Address: {props.brewery.street} </p>
+        <p>{props.brewery.phone.slice(0, 3)}-{props.brewery.phone.slice(3, 6)}-{props.brewery.phone.slice(6, 9)}</p>
+        <p>Address: {props.brewery.street}</p>
         <p>{props.brewery.city}, CO {props.brewery.postal_code}</p>
         <p>Brewery Link: {props.brewery.url}</p>
       <button type='button'>Add to Favorites</button>
