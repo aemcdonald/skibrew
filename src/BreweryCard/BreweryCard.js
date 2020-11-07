83332a18d0dbc5a614bbf48b60070e409fdc999a
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-06 22:00:30 -0700

    Edit BreweryCard Address

--- src/BreweryCard/BreweryCard.js
+++ src/BreweryCard/BreweryCard.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './BreweryCard.css';
 
 const BreweryCard = (props) => {
+  console.log(props.brewery);
   return(
     <section>
-      <h4>{props.name}</h4>
-        <p>Brewery Address: {props.street}</p>
-        <p>{props.state}</p>
-        <p>{props.postal_code}</p>
-        <p>Brewery Link: {props.url}</p>
+      <h4>{props.brewery.name}</h4>
+        <p>Address: {props.brewery.street} </p>
+        <p>{props.brewery.city}, CO {props.brewery.postal_code}</p>
+        <p>Brewery Link: {props.brewery.url}</p>
       <button type='button'>Add to Favorites</button>
     </section>
   )
