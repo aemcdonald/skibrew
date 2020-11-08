84429b591fa92e39b44987a97d142549ae78069c
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-07 17:47:38 -0700

    Write unit test for navigation links

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -11,8 +11,21 @@ describe('App', () => {
         <App />
       </BrowserRouter>
     )
-    
+
     const title = screen.getByText('Ski Brew');
     expect(title).toBeInTheDocument();
+  });
+
+  it('Should display home and favorites links', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    )
+    const homeLink = screen.getByText('Home');
+    const favsLink = screen.getByText('Favorites');
+
+    expect(homeLink).toBeInTheDocument();
+    expect(favsLink).toBeInTheDocument();
   });
 });
