844b1b6a48c1ad311ae258e84e21112852ce8da7
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-07 17:43:15 -0700

    Write unit test for App

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe('App', () => {
-  it('Should pass an initial test', () => {
-    expect(true).toBe(true);
-  })
+  it('Should display a title', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    )
+    
+    const title = screen.getByText('Ski Brew');
+    expect(title).toBeInTheDocument();
+  });
 });
-  
