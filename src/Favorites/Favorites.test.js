a39da50bc08521a18d088e72c46a72b928a81ee1
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-07 17:30:55 -0700

    Create unit test for Favorites page title

--- src/Favorites/Favorites.test.js
+++ src/Favorites/Favorites.test.js
@@ -4,7 +4,18 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe('Favorites', () => {
-  it('Should pass an initial test', () => {
-    expect(true).toBe(true);
+  it('Should display a title', () => {
+    const mockFavs = [
+      { id: 1, name: 'Brewery1', phone: '1112223333', street: '123 Abc St', city: 'Mars', postal_code: '12345', website_url: 'URL1' },
+      { id: 2, name: 'Brewery2', phone: '1112224444', street: '456 Def St', city: 'Saturn', postal_code: '67890', website_url: 'URL2' }
+    ]
+    const { getByText } = render(
+      <Favorites
+        favorites={mockFavs}
+      />
+    )
+
+    const title = screen.getByText('Favorites');
+    expect(title).toBeInTheDocument();
   })
 });
