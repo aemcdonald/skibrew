fa65ce37706fb0df3926b0d2a43736d48ad90ee0
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-07 17:23:24 -0700

    Write integration test for Brewery Container

--- src/BreweryContainer/BreweryContainer.test.js
+++ src/BreweryContainer/BreweryContainer.test.js
@@ -7,7 +7,7 @@ describe('BreweryContainer', () => {
   it('Should display a title', () => {
     const mockBreweries = [
       { id: 1, name: 'Brewery1', phone: '1234567890', street: '123 Abc St', city: 'Mars', postal_code: '12345', website_url: 'URL' },
-      { id: 2, name: 'Storm Peak Brewing', phone: '1234567890', street: '123 Abc St', city: 'Steamboat Springs', postal_code: '12345', website_url: 'URL' }
+      { id: 2, name: 'Brewery2', phone: '1234567890', street: '456 Def St', city: 'Saturn', postal_code: '67890', website_url: 'URL' }
     ]
 
     const { getByText } = render(
@@ -17,5 +17,31 @@ describe('BreweryContainer', () => {
 
     const title = screen.getByText('Apres Ski Brewery Choices')
     expect(title).toBeInTheDocument();
+  });
+
+  it('Should display brewery information passed to it', () => {
+    const mockBreweries = [
+      { id: 1, name: 'Brewery1', phone: '1234567890', street: '123 Abc St', city: 'Mars', postal_code: '12345', website_url: 'URL' },
+      { id: 2, name: 'Brewery2', phone: '1234567890', street: '456 Def St', city: 'Saturn', postal_code: '67890', website_url: 'URL' }
+    ]
+
+    const { getByText } = render(
+      <BreweryContainer
+        breweries={mockBreweries}
+      />)
+
+    const brewery1Name = screen.getByText('Brewery1');
+    const brewery2Name = screen.getByText('Brewery2');
+    const brewery1Street = screen.getByText('Address: 123 Abc St');
+    const brewery2Street = screen.getByText('Address: 456 Def St');
+    const brewery1City = screen.getByText('Mars, CO 12345');
+    const brewery2City = screen.getByText('Saturn, CO 67890');
+
+    expect(brewery1Name).toBeInTheDocument();
+    expect(brewery2Name).toBeInTheDocument();
+    expect(brewery1Street).toBeInTheDocument();
+    expect(brewery2Street).toBeInTheDocument();
+    expect(brewery1City).toBeInTheDocument();
+    expect(brewery2City).toBeInTheDocument();
   });
 });
