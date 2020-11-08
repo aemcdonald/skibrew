e60d239da3b0a9f3477faf8f78697fbdfbb7783a
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-07 17:00:20 -0700

    Create test for page title

--- src/BreweryContainer/BreweryContainer.test.js
+++ src/BreweryContainer/BreweryContainer.test.js
@@ -4,7 +4,18 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe('BreweryContainer', () => {
-  it('Should pass an initial test', () => {
-    expect(true).toBe(true);
-  })
+  it('Should display a title', () => {
+    const mockBreweries = [
+      { id: 1, name: 'Brewery1', phone: '1234567890', street: '123 Abc St', city: 'Mars', postal_code: '12345', website_url: 'URL' },
+      { id: 2, name: 'Storm Peak Brewing', phone: '1234567890', street: '123 Abc St', city: 'Steamboat Springs', postal_code: '12345', website_url: 'URL' }
+    ]
+
+    const { getByText } = render(
+      <BreweryContainer
+        breweries={mockBreweries}
+      />)
+
+    const title = screen.getByText('Apres Ski Brewery Choices')
+    expect(title).toBeInTheDocument();
+  });
 });
