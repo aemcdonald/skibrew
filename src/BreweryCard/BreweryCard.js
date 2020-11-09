3e5499e3c1e5fbe5e059eca468c2ef9ab439c30f
Ashley McDonald <ashley.mcdonald.e@gmail.com>
2020-11-09 14:18:46 -0700

    Fix tab

--- src/BreweryCard/BreweryCard.js
+++ src/BreweryCard/BreweryCard.js
@@ -11,7 +11,7 @@ const BreweryCard = (props) => {
         <p>{props.brewery.city}, CO {props.brewery.postal_code}</p>
         <p>Brewery Link: <a href={props.brewery.website_url}target='_blank' rel='noreferrer'>View Website</a></p>
         {!props.deleteFav &&
-      <button className='fav-btn' data-testid='favBtn' type='button'onClick={() => props.handleClick(props.brewery)}>Add to Favorites</button>}
+          <button className='fav-btn' data-testid='favBtn' type='button'onClick={() => props.handleClick(props.brewery)}>Add to Favorites</button>}
       {props.deleteFav &&
     <button className='delete-fav-btn' data-testid='delete-favBtn' type='button'onClick={() => props.deleteFav(props.brewery)}>Remove from Favorites</button>}
     </section>
